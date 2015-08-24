2eb90ba89600b4da7f838f8a9f3f936697d3719d
Julian Thijssen <Nimthora@gmail.com>
2015-08-25 01:56:15 +0200

    Changed start button to Space

--- Assets/Scripts/Controllers/TitleController.cs
+++ Assets/Scripts/Controllers/TitleController.cs
@@ -10,7 +10,7 @@ public class TitleController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.anyKey) {
+		if (Input.GetKey (KeyCode.Space)) {
 			ToGameScreen();
 		}
 	}
